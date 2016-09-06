6d09c2b4b4612ef8c9c5ae736da16200c15fc3d9
457040086 <457040086@qq.com>
2016-09-06 03:23:05 -0500

    Add files via upload

---
+++ js/blogjavascrapt.js
@@ -0,0 +1,40 @@
+
+$(function(){
+$(".hei").css("backgroundColor","black");
+$(".hei").css("color","white");
+$(".bg").css("backgroundColor","white");
+$(".pad").css("padding","15px");
+$("#zero").css("width","50px");
+$(".bg1").css("backgroundColor","black");
+$(".zi").css("color","white");
+$(".bg2").css("backgroundColor","#616161");
+$(".bg2").css("height","150px");
+$(".border").css("border-top","0px");
+$(".bg3").css("backgroundColor","#f1f1f1");
+$(".a").css('box-shadow',"0px 5px 15px gray");
+$(".b").css('box-shadow',"1px 1px 1px gray");
+$(".badge").css("backgroundColor","black");
+$(".c").css("width","100%");
+$(".bg4").css("backgroundColor","#444");
+$(".wei").css("margin-left","15px");
+$(".wei").css("margin-top","30px");
+$(".radius").css("border-radius","0px");
+$(".btn").css("outline","none");
+});
+$(".b").mouseover(function(){
+$(this).css('box-shadow',"1px 1px 15px gray");
+$(this).css('border-color',"black");
+});
+$(".b").mouseout(function(){
+$(this).css('box-shadow',"0px 0px 0px gray");
+$(this).css('border-color',"#c0c0c0");
+});
+
+
+$(".move").mouseover(function(){
+$(this).css('backgroundColor',"#ccc");
+
+});
+$(".move").mouseout(function(){
+$(this).css('backgroundColor',"white");
+});
